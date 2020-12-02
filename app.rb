dd59d0ec76f954ad8ac33c33db3d138742da81cf
bengroves <bengroves15@gmail.com>
2020-12-02 17:17:02 -0500

    commit tree 89215addc4d52da76c143a00b4e9af66cf4ba25d

--- app.rb
+++ app.rb
@@ -1,6 +1,26 @@
 require 'sinatra/base'
 
 class App < Sinatra::Base
+    get '/' do
+        "Hello World"
+    end
+
+    get '/newteam' do
+        erb :newteam
+    end 
+
+    post '/team' do
+        @teamname = params[:name]
+        @coachname = params[:coach]
+        @pointguard = params[:pg]
+        @shootingguard = params[:sg]
+        @shortforward = params[:sf]
+        @powerforward = params[:pf]
+        @center = params[:c]
+
+        erb :team
+
+    end 
 
 
 end
